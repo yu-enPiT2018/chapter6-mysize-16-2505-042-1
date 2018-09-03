121e6eb54f034cb5003afdd4bd496a86612c0912
16-2505-042-1 <w042ff@yamaguchi-u.ac.jp>
2018-09-03 11:43:30 +0900

    xxx

--- app/src/main/java/com/example/enpit_p12/mysize/HeightActivity.kt
+++ app/src/main/java/com/example/enpit_p12/mysize/HeightActivity.kt
@@ -1,10 +1,9 @@
 package com.example.enpit_p12.mysize
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.view.ViewParent
 import android.widget.AdapterView
 import android.widget.RadioButton
 import android.widget.SeekBar
