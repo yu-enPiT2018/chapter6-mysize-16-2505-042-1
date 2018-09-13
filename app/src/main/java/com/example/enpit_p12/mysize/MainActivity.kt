4167622be2fa7ef9c4cf348f53ebd544a9240e88
16-2505-042-1 <w042ff@yamaguchi-u.ac.jp>
2018-09-13 16:21:46 +0900

    ibuio

--- app/src/main/java/com/example/enpit_p12/mysize/MainActivity.kt
+++ app/src/main/java/com/example/enpit_p12/mysize/MainActivity.kt
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
-class MainActivity : AppCompatActivity() { //ufufufyulfygyulgieuauigphe
+class MainActivity : AppCompatActivity() { //ufufufyulfygyulgieuauigphebuvyvvou
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
